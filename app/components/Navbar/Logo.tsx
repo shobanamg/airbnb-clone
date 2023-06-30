c9debebcebf16e17ba7cf1561ff7e199608f9fc1
shobana_mathiarul <mathiarulshobana@gmail.com>
2023-06-30 22:38:52 +0200

    added logo component

---
+++ app/components/Navbar/Logo.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+const Logo: React.FC = () => {
+  const router = useRouter();
+  return (
+    <Image
+      onClick={() => router.push('/')}
+      className="hidden cursor-pointer md:block"
+      src="/images/logo.png"
+      height="100"
+      width="100"
+      alt="Logo"
+    />
+  );
+};
+export default Logo;
