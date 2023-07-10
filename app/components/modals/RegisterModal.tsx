335217836ad3441c9c563ea6f5eb28659f98a76c
shobana_mathiarul <mathiarulshobana@gmail.com>
2023-07-10 14:30:46 +0200

    updated RegisterModal component with neccessary changes

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -5,8 +5,11 @@ import { useState } from 'react';
 import type { FieldValues, SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
+import { AiFillGithub } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
 
 import useRegisterModal from '../../hooks/useRegisterModal';
+import Button from '../Button';
 import Heading from '../Heading';
 import Input from '../Input';
 import Modal from './Modal';
@@ -54,6 +57,64 @@ const RegisterModal = () => {
         errors={errors}
         required
       />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="mt-3 flex flex-col gap-4">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div
+        className="
+          mt-4
+          text-center
+          font-light
+          text-neutral-500
+        "
+      >
+        <p>
+          Already have an account?
+          <span
+            onClick={registerModal.onClose}
+            className="
+              cursor-pointer
+              text-neutral-800
+              hover:underline
+            "
+          >
+            {' '}
+            Log in
+          </span>
+        </p>
+      </div>
     </div>
   );
 
@@ -66,6 +127,7 @@ const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div> Welcome to Airbnb</div>;
+  return <div />;
 }
