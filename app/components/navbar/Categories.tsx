f67a1afc7a4fe51690c805d302bf9932ee185e69
shobana_mathiarul <mathiarulshobana@gmail.com>
2023-07-17 13:41:03 +0200

    implemented categories bar hiding feature because we want to see it only on main page.

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,6 +1,19 @@
-import { GiWindmill } from 'react-icons/gi';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { BsSnow } from 'react-icons/bs';
+import { FaSkiing } from 'react-icons/fa';
+import {
+  GiBarn,
+  GiBoatFishing,
+  GiCactus,
+  GiCastle,
+  GiCaveEntrance,
+  GiForestCamp,
+  GiIsland,
+  GiWindmill,
+} from 'react-icons/gi';
+import { IoDiamond } from 'react-icons/io5';
 import { MdOutlineVilla } from 'react-icons/md';
-import { TbBeach, TbMountain } from 'react-icons/tb';
+import { TbBeach, TbMountain, TbPool } from 'react-icons/tb';
 
 import CategoryBox from '../CategoryBox';
 import Container from '../Container';
@@ -26,9 +39,73 @@ export const categories = [
     icon: TbMountain,
     description: 'This property is in the countryside!',
   },
+  {
+    label: 'Pools',
+    icon: TbPool,
+    description: 'This is property has a beautiful pool!',
+  },
+  {
+    label: 'Islands',
+    icon: GiIsland,
+    description: 'This property is on an island!',
+  },
+  {
+    label: 'Lake',
+    icon: GiBoatFishing,
+    description: 'This property is near a lake!',
+  },
+  {
+    label: 'Skiing',
+    icon: FaSkiing,
+    description: 'This property has skiing activies!',
+  },
+  {
+    label: 'Castles',
+    icon: GiCastle,
+    description: 'This property is an ancient castle!',
+  },
+  {
+    label: 'Caves',
+    icon: GiCaveEntrance,
+    description: 'This property is in a spooky cave!',
+  },
+  {
+    label: 'Camping',
+    icon: GiForestCamp,
+    description: 'This property offers camping activities!',
+  },
+  {
+    label: 'Arctic',
+    icon: BsSnow,
+    description: 'This property is in arctic environment!',
+  },
+  {
+    label: 'Desert',
+    icon: GiCactus,
+    description: 'This property is in the desert!',
+  },
+  {
+    label: 'Barns',
+    icon: GiBarn,
+    description: 'This property is in a barn!',
+  },
+  {
+    label: 'Lux',
+    icon: IoDiamond,
+    description: 'This property is brand new and luxurious!',
+  },
 ];
 
 const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get('category');
+  const pathname = usePathname();
+  const isMainPage = pathname === '/';
+
+  if (!isMainPage) {
+    return null;
+  }
+
   return (
     <Container>
       <div
@@ -42,7 +119,12 @@ const Categories = () => {
         "
       >
         {categories.map((item) => (
-          <CategoryBox key={item.label} label={item.label} icon={item.icon} />
+          <CategoryBox
+            key={item.label}
+            label={item.label}
+            selected={category === item.label}
+            icon={item.icon}
+          />
         ))}
       </div>
     </Container>
