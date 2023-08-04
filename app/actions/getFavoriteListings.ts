ca58251b6de061936bbad4101fd4229ee564c06b
shobana_mathiarul <mathiarulshobana@gmail.com>
2023-08-04 21:20:23 +0200

    added Favorites page, here we load favorite listings using server component

---
+++ app/actions/getFavoriteListings.ts
@@ -0,0 +1,31 @@
+import prisma from '../libs/prismadb';
+import getCurrentUser from './getCurrentUser';
+
+const getFavoriteListings = async () => {
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+      return [];
+    }
+
+    const favorites = await prisma.listing.findMany({
+      where: {
+        id: {
+          in: [...(currentUser.favoriteIds || [])],
+        },
+      },
+    });
+
+    const safeFavorites = favorites.map((favorite) => ({
+      ...favorite,
+      createdAt: favorite.createdAt.toString(),
+    }));
+
+    return safeFavorites;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
+
+export default getFavoriteListings;
