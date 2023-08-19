170ef0a4ea31abf118c99e2595acfbd067fb0fce
shobana_mathiarul <mathiarulshobana@gmail.com>
2023-08-19 23:21:51 +0200

    Fix build issue

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -81,12 +81,10 @@ const getListings = async (params: IListingsParams) => {
         createdAt: 'desc',
       },
     });
-    const safeListings = listings.map((listing: any) => ({
+    return listings.map((listing: any) => ({
       ...listing,
       createdAt: listing.createdAt.toISOString(),
     }));
-
-    return safeListings;
   } catch (error: any) {
     throw new Error(error);
   }
