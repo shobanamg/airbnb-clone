e8307c141a8e25dcd9a5f967019b4381a3c6786c
shobana_mathiarul <mathiarulshobana@gmail.com>
2023-08-19 22:47:08 +0200

    fixed type error

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -4,7 +4,11 @@ declare global {
   let prisma: PrismaClient | undefined;
 }
 
+// @ts-ignore
 const client = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
+if (process.env.NODE_ENV !== 'production') {
+  // @ts-ignore
+  globalThis.prisma = client;
+}
 
 export default client;
